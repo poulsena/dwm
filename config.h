59101b644a47c8f7ab9e8e25eb6b41e2c6f87141
Andreas Poulsen <andreas@poulsena.dk>
2024-02-03 12:30:40 +0100

    decreased font sizes

--- config.h
+++ config.h
@@ -17,13 +17,13 @@ static const unsigned int gappoh =
 static const unsigned int gappov =
     10; /* vert outer gap between windows and screen edge */
 static int smartgaps =
-    1;                        /* 1 means no outer gap when there is only one window */
+    1; /* 1 means no outer gap when there is only one window */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
 static const char *fonts[] = {
-    "monospace:size=12",
-    "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"};
-static const char dmenufont[] = "monospace:size=12";
+    "monospace:size=8",
+    "NotoColorEmoji:pixelsize=8:antialias=true:autohint=true"};
+static const char dmenufont[] = "monospace:size=8";
 static const char col_gray1[] = "#282828";
 static const char col_gray2[] = "#504945";
 static const char col_gray3[] = "#bdae93";
@@ -60,8 +60,8 @@ static const int attachbelow = 1;
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
 
-#define FORCE_VSPLIT \
-    1 /* nrowgrid layout: force two clients to always split vertically */
+#define FORCE_VSPLIT                                                           \
+  1 /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -84,24 +84,24 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define TAGKEYS(KEY, TAG)                                          \
-    {MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
-        {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
-        {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
-        {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+#define TAGKEYS(KEY, TAG)                                                      \
+  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
+      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
+      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
+      {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd)                                           \
-    {                                                        \
-        .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL } \
-    }
+#define SHCMD(cmd)                                                             \
+  {                                                                            \
+    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
+  }
 
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1,
-    "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL};
+    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {TERMINAL, NULL};
 
 static const Key keys[] = {
